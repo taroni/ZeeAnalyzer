c3f1be3ae4622f5f2f8b3d0cbf32c52fe0a48128
Silvia <Silvia.Taroni@cern.ch>
2017-11-10 11:46:59 +0100

    changing dataset

--- ZeeAnalyzer/test/crab_ElectronPlots.py
+++ ZeeAnalyzer/test/crab_ElectronPlots.py
@@ -10,7 +10,7 @@ config.General.transferLogs = True
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = 'runElectronPlots_v1.py'
 
-config.Data.inputDataset = '/GenericTTbar/HC-CMSSW_5_3_1_START53_V5-v1/GEN-SIM-RECO'
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISummer17DRPremix-92X_upgrade2017_realistic_v10_ext1-v2/AODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 1000
